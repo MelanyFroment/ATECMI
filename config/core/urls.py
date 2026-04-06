5282111715fe33aefa2ed6d50bf181e98b9db54c
MelanyF <melany.from@yahoo.com>
2026-04-06 12:06:16 +0200

    create page convoyage produit + RESPONSIVE

--- config/core/urls.py
+++ config/core/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('depalettisation/', views.depalettisation, name='depalettisation'),
     path('palettisation/', views.palettisation, name='palettisation'),
+    path('convoyage-produit/', views.convoyage_produit, name='convoyage_produit'),
+    path('encartonnage/', views.encartonnage, name='encartonnage'),
+    path('convoyage-colis/', views.convoyage_colis, name='convoyage_colis'),
+    path('integration/', views.integration, name='integration'),
 ]
--- config/core/views.py
+++ config/core/views.py
@@ -14,3 +14,15 @@ def depalettisation(request):
 
 def palettisation(request):
     return render(request, 'core/palettisation.html')
+
+def convoyage_produit(request):
+    return render(request, "core/convoyage-produit.html")
+
+def encartonnage(request):
+    return render(request, "core/encartonnage.html")
+
+def convoyage_colis(request):
+    return render(request, "core/convoyage-colis.html")
+
+def integration(request):
+    return render(request, "core/integration.html")
