bcdcb8779a3c073f79bebb0fc90512adf8b2db96
MelanyF <melany.from@yahoo.com>
2026-04-07 11:51:10 +0200

    update css style.css and setting.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ktv_62d2#q22jt)2llosw4t*3*mwy+^!##b&3id@(d%_uq(z$l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
